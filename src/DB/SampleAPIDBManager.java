34562c5ddcd37d66cfa59cfc05dd257014261a75
mekapiku <mekapiku@gmail.com>
2015-07-28 22:00:24 +0900

    ・DB用クラス追加

---
+++ src/DB/SampleAPIDBManager.java
@@ -0,0 +1,8 @@
+package DB;
+
+/**
+ * Created by mitsuyasu on 2015/07/28.
+ */
+public class SampleAPIDBManager {
+    //http://wiki4.caucho.com/Java_EE_Servlet/JSP_tutorial_:_Adding_MySQL_and_JDBC_to_bookstore_example#Create_Repository_class_that_implements_BookRepository
+}
